4ba56d7778e6ab7b159b623810e405f18251d99b
user <procsitehcom2009@yandex.ru>
2021-10-08 17:57:58 +0200

    Добавил файл index.php для лекции

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$socket = fsockopen("test.shelenkov.com", 80, $errorCode, $errorString);
+if (!$socket)
+{
+    echo "$errorCode ($errorString)<br />\n";
+    die();
+}
+
+$result  = "" . "\r\n";
+$result .= "" . "\r\n";
+$result .= "" . "\r\n";
+$result .= "\r\n";
+
+fwrite($socket, $result);
+while (!feof($socket))
+{
+    echo fgets($socket);
+}
+fclose($socket);
