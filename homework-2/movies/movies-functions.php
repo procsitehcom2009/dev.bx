5596c1e7a4c334e01397a84966bb91ba1d6b9b6d
user <procsitehcom2009@yandex.ru>
2021-10-17 11:34:18 +0200

    Добавлен вывод номера по порядку для выводимого списка фильмов

--- homework-2/movies/movies-functions.php
+++ homework-2/movies/movies-functions.php
@@ -2,15 +2,16 @@
 
 function printMovies(array $movies): void
 {
+	$serialNumber = (int) 0;
     foreach ($movies as $movie)
     {
-        printMessage(formatMovie($movie));
+        printMessage(formatMovie($movie,++$serialNumber));
     }
 }
 
-function formatMovie(array $movie): string
+function formatMovie(array $movie, int $serialNumber): string
 {
-    return "{$movie['title']} ({$movie['release_year']}), {$movie['age_restriction']}+. Rating - {$movie['rating']}";
+    return "{$serialNumber}. {$movie['title']} ({$movie['release_year']}), {$movie['age_restriction']}+. Rating - {$movie['rating']}";
 }
 
 function filterAvailableMovies(int $age, array $movies): array
