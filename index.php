75f9cb5264d36dbfa532746d42d6af65ae861ca3
user <procsitehcom2009@yandex.ru>
2021-10-17 11:20:30 +0200

    Перенес все файлы в папку homework-2. Поменял метод restrictionsMoviesAge на filterAvailableMovies и также переменные внутри метода.

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/** @var array $movies */
-require "./movies/movies.php";
-require "./movies/movies-functions.php";
-require "functions.php";
-
-$age = readline("Введите ваш возраст:");
-validateAge($age,$movies);
