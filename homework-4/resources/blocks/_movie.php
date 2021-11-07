fb19ea38e0f999bae5c6192a6ac0195b70a73441
user <procsitehcom2009@yandex.ru>
2021-11-08 00:23:11 +0200

    Изменение папки ресурсов

---
+++ homework-4/resources/blocks/_movie.php
@@ -0,0 +1,21 @@
+<?php
+/** @var array $movie */
+?>
+<div class="movie-list--item">
+	<div class="movie-list--item-image" style="background-image: url(data/images/1.jpg)"></div>
+	<div class="movie-list--item-head">
+		<div class="movie-list--item-title">Не время умирать (2021)</div>
+		<div class="movie-list--item-subtitle">No Time to Die</div>
+		<div class="movie-list--item-wrapper"></div>
+	</div>
+	<div class="movie-list--item-description">Джеймс Бонд оставил оперативную службу и наслаждается спокойной жизнью на
+		Ямайке. Все меняется, когда на острове появляется его старый друг Феликс Лейтер из ЦРУ с просьбой о помощи.
+	</div>
+	<div class="movie-list--item-bottom">
+		<div class="movie-list--item-time">
+			<div class="movie-list--item-time--icon"></div>
+			163 мин. / 02:43
+		</div>
+		<div class="movie-list--item-info">боевик, триллер, приключения</div>
+	</div>
+</div>
